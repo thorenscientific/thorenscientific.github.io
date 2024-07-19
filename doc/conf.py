4cd49a889369d637dae031c61659b1511609c7ba
Thoren <thorenscientific@gmail.com>
2024-07-18 22:06:43 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -24,15 +24,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-
-
-
-
 html_theme = 'sphinx_rtd_theme'
 # html_theme = 'cloud'
 html_static_path = ['_static']
-html_logo = '_static/nerd_points_at_shiny_object.jpg'
-
-html_theme_options = {'description':'By Mark Thoren - thorenscientific@gmail.com',
-                      'logo_name': True,
-                      'logo_only': False}
+html_logo = 'nerd_points_at_shiny_object.png'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,}
